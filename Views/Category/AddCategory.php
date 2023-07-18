91f3ce297879014e2098539dbcfce2a06fa090b8
NinoSayo <119102188+NinoSayo@users.noreply.github.com>
2023-07-18 14:54:13 +0700

    Update AddCategory.php

---
+++ Views/Category/AddCategory.php
@@ -0,0 +1,62 @@
+<?php
+require_once("../../Controllers/categoryController.php");
+
+$id = $name = $description = $image = '';
+
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $categories = getCategoriesByID($id);
+        $name = $categories['category_name'];
+        $description = $categories['category_descriptions'];
+        $image = $categories['category_image'];
+    }
+    if(isset($_POST) && !empty($_POST)){
+        if(isset($_POST['name'])){
+            $name = $_POST['name'];
+        }
+        if(isset($_POST['description'])){
+            $description = $_POST['description'];
+        }
+        if(isset($_FILES['image']) && !empty($_FILES['image']['name'])){
+            $image = "Images/category/".basename($_FILES['image']['name']);
+            $imagePath = "../../Assets/Images/category/".basename($_FILES['image']['name']);
+            move_uploaded_file($_FILES['image']['tmp_name'],$imagePath);
+        }
+
+        if($id == ''){
+            addCategory($name,$image,$description);
+        }else{
+            updateCategory($id,$name,$image,$description);        
+        }
+
+        header("Location: Category.php");
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method = "POST" enctype="multipart/form-data">
+        <label for="name">Name:</label>
+        <input type="text" name="name" value="<?=$name?>" required>
+        <br>
+        <label for="image">Category Image:</label>
+        <input type="file" name="image" id="input-image">
+        <?php
+        if($image != ''){
+            echo '<img src="../../Assets/'.$image.'" width="100px" height="100px">';
+        }
+        ?>
+        <br>
+        <label for="description"></label>
+        <textarea name="description" id="" cols="30" rows="10"><?=$description?></textarea>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
