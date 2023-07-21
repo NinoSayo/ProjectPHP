7be20bb8235c9a1205c26971c76faa4be45bc1ed
NinoSayo <119102188+NinoSayo@users.noreply.github.com>
2023-07-21 15:35:21 +0700

    V1

--- Controllers/productController.php
+++ Controllers/productController.php
@@ -11,30 +11,14 @@ function getProductsByID($id){
     return executeSingleResult($sql);
 }
 
-function addProduct($name,$quantity,$price,$description,$cateID){
-   $sql = "INSERT INTO product(product_name,product_quantity,product_price,product_descriptions,category_id) VALUES ( '$name' , $quantity , $price , $description , $cateID )";
-   return execute($sql);
-}
-
-function updateProduct($id,$name,$quantity,$price,$description,$cateID){
-    $sql = "UPDATE product SET product_name = '$name',product_quantity = $quantity , product_price = $price , product_descriptions = '$description' , category_id = $cateID WHERE product_id = $id";
+function addProduct($name,$quantity,$price,$description,$category_id){
+    $sql = "INSERT INTO product (product_name,product_quantity,product_price,product_descriptions,category_id) VALUES ('$name',$quantity,$price,'$description',$category_id)";
     return execute($sql);
 }
 
-function deleteProduct($id){
-    $products = getProductsByID($id);
-
-    if($products && !empty($products)){
-        $imagePath = "../../Assets/".$products['product_image'];
-        if(file_exists($imagePath) && is_file($imagePath)){
-            unlink ($imagePath);
-        }
-        $sql = "DELETE FROM product WHERE product_id = $id";
-        return execute($sql);
-    }else{
-        return false;
-    }
-
+function updateProduct($id,$name,$quantity,$price,$description,$category_id){
+    $sql = "UPDATE product SET product_name = '$name',product_quantity = $quantity,product_price = $price,product_descriptions = '$description',category_id = $category_id WHERE product_id = $id";
+    return execute($sql);
 }
 
 ?>
