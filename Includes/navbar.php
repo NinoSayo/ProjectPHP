5a4adf493ea4fd79f61e177ea5fa4f994ed9a4d5
NinoSayo <119102188+NinoSayo@users.noreply.github.com>
2023-08-28 18:05:19 +0700

    Update navbar.php

--- Includes/navbar.php
+++ Includes/navbar.php
@@ -3,23 +3,21 @@
   <div class="container">
     <a class="navbar-brand" href="index.php">Vouge Store</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="oi oi-menu"></span> Menu
+      <span class="oi oi-menu"></span>Menu
     </button>
     <div class="collapse navbar-collapse" id="ftco-nav">
       <ul class="navbar-nav ml-auto">
-        <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item active"><a id="homeLink" href="index.php" class="nav-link">Home</a></li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Catalog</a>
           <div class="dropdown-menu" aria-labelledby="dropdown04">
-            <a class="dropdown-item" href="shop.php">Shop</a>
-            <a class="dropdown-item" href="cart.php">Cart</a>
-            <a class="dropdown-item" href="checkout.php">Checkout</a>
+            <a id="shopLink" class="dropdown-item" href="shop.php">Shop</a>
           </div>
         </li>
         <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
         <li class="nav-item"><a href="blog.php" class="nav-link">Blog</a></li>
         <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
-        <li class="nav-item cta cta-colored"><a href="cart.php" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
+        <li class="nav-item cta cta-colored"><a id="cartLink" href="cart.php" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
       </ul>
     </div>
   </div>
@@ -76,7 +74,7 @@
 
   var navigationLink = {
     "homeLink": "/index.php",
-    "collectionLink" : "/categories.php",
+    "shopLink" : "/shop.php",
     "registerLink" : "/register.php",
     "loginLink" : "/login.php",
     "cartLink" : "/cart.php",
